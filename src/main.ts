50d56cc7e7fbcce9373a32c6c2f71ec94d800e95
TinhNguyenTB <tinh271103@gmail.com>
2025-03-15 15:54:36 +0700

    transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { TransformInterceptor } from '@/core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   const port = configService.get('PORT');
+  const reflector = app.get(Reflector);
+
+  // Enable interceptor globally
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
