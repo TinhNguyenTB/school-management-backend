3af14617598a501baecd8ae42c763c4e5573f912
TinhNguyenTB <tinh271103@gmail.com>
2025-03-15 15:49:19 +0700

    config CORS and api version

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +14,23 @@ async function bootstrap() {
     forbidNonWhitelisted: true
   }));
 
+  app.setGlobalPrefix('api', { exclude: [''] })
+
+  // version 1
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1']
+  })
+
+  //config cors
+  app.enableCors(
+    {
+      "origin": true,
+      "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+      "preflightContinue": false,
+      credentials: true
+    }
+  );
 
   await app.listen(port);
 }
